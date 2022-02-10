1368758f6a81b221d54c29ec723de5aa86d5bafb
Carlos <cmartinez1@ucam.edu>
2022-02-10 11:49:56 +0100

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -12,7 +12,7 @@
 
 error=0		  
 paraMError="" 	  
-txtErrror=""  	  
+txtError=""  	  
 informe=""	  
 fecha=$(date +"%Y-%m-%d")
 pathSL="MetaScreener/login_node/"
@@ -28,9 +28,9 @@ fi
 
 function f_help(){
 	source ${pathSL}help.sh $1
-	if [ ! -z "$txtErrror" ];then 
+	if [ ! -z "$txtError" ];then 
 		echo ""
-		echo -e "${RED}ERROR: ${BROWN}"$txtErrror"${NONE}"
+		echo -e "${RED}ERROR: ${BROWN}"$txtError"${NONE}"
 		echo ""
 	fi
 	exit
@@ -46,18 +46,18 @@ function f_Error()
 	
 	laError="MetaScreener.sh,"
 	case $error in
-		1)  txtErrror="$laError Incorrect docking program"   ; f_help;;
-		2)  txtErrror="$laError Incorrect docking technique for that program"; f_help;;
-		3)  txtErrror="$laError Extension not valid for docking program"; f_help;;
-		4)  txtErrror="$laError Ligand or protein file empty"; f_help;;
-		5)  txtErrror="$laError Cluster hostname not found"; f_help;;
-		6)  txtErrror="$laError No docking option found"; f_help;;
-		7)  txtErrror="$laError numJobs, x, y, or z are empty"; f_help;;
-		8)  txtErrror="$laError Protein file, ligand folder or ligand file DOES NOT EXIST";f_help;;
-		9)  txtErrror="$laError You cannot use hyphens (-) in protein or ligand file names";f_help;;
-		10) txtErrror="$laError Wrong queue manager, ";f_help;;
-		11) txtErrror="$laError Renice parameter wrong, check help";f_help;;
-		12) txtErrror="$laError Unknown Parameter $paraMError, check help";f_help;;
+		1)  txtError="$laError Incorrect docking program"   ; f_help;;
+		2)  txtError="$laError Incorrect docking technique for that program"; f_help;;
+		3)  txtError="$laError Extension not valid for docking program"; f_help;;
+		4)  txtError="$laError Ligand or protein file empty"; f_help;;
+		5)  txtError="$laError Cluster hostname not found"; f_help;;
+		6)  txtError="$laError No docking option found"; f_help;;
+		7)  txtError="$laError numJobs, x, y, or z are empty"; f_help;;
+		8)  txtError="$laError Protein file, ligand folder or ligand file DOES NOT EXIST";f_help;;
+		9)  txtError="$laError You cannot use hyphens (-) in protein or ligand file names";f_help;;
+		10) txtError="$laError Wrong queue manager, ";f_help;;
+		11) txtError="$laError Renice parameter wrong, check help";f_help;;
+		12) txtError="$laError Unknown Parameter $paraMError, check help";f_help;;
 	esac
 }
 #___________________________________________________________________________________________________________________________________________________________#
@@ -77,7 +77,7 @@ if [ "$mode_test" != "N/A" ];then
 		if [ -f "${pathSL}/test_params/test_params_${option}_${software}.sh" ]; then
 			source ${pathSL}/test_params/test_params_${option}_${software}.sh
 		else
-			txtErrror="No parameters found for the test ${option} ${software}"
+			txtError="No parameters found for the test ${option} ${software}"
 			f_help
 		fi
 	else
