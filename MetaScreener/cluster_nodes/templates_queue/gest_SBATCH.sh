3abbb39d8293c5521f74e50d87fb3ec1739fbaba
Carlos <cmartinez1@ucam.edu>
2022-07-05 11:01:15 +0200

    Update gest_SBATCH.sh

--- MetaScreener/cluster_nodes/templates_queue/gest_SBATCH.sh
+++ MetaScreener/cluster_nodes/templates_queue/gest_SBATCH.sh
@@ -13,8 +13,8 @@ if [ "$project" != "N/A" ];then
 	echo ${queue_direc_project}${project}				>>$name_template_job
 fi
 
-echo "#SBATCH --output=${folder_out_jobs}${outJob}.out"	>>$name_template_job
-echo "#SBATCH --error=${folder_out_jobs}${outJob}.err"	>>$name_template_job
+echo "#SBATCH --output=${folder_out_jobs}${outJob}-%j.out"    >>$name_template_job
+echo "#SBATCH --error=${folder_out_jobs}${outJob}-%j.err"    >>$name_template_job
 echo "#SBATCH -p "${queue}>>$name_template_job
 echo "#SBATCH -J ${name_job}"							>>$name_template_job
 echo "#SBATCH --time=$time_job"							>>$name_template_job
