ee4ef1ab27d085656a64a61f168b968676e04a40
Carlos <cmartinez1@ucam.edu>
2022-11-03 09:31:18 +0100

    Update scriptDC.sh

--- MetaScreener/cluster_nodes/execute_scripts/scriptDC.sh
+++ MetaScreener/cluster_nodes/execute_scripts/scriptDC.sh
@@ -4,7 +4,7 @@ DRAGON_CFG=${DRAGON_PATH}cfg_template.drs
 DRAGON_RUN=${DRAGON_PATH}dragon6shell
 TOKEN_INPUT="MOL_FILE.SDF"
 TOKEN_OUTPUT="SAVE_MOL.CSV"
-SCRIPT_FILETER_COLUMNNS=${path_extra_metascreener}used_by_metascreener/filter_columns_dc.py
+SCRIPT_FILTER_COLUMNNS=${path_extra_metascreener}used_by_metascreener/filter_columns_dc.py
 DRAGON_OUT=${out_grid}.csv
 FILTER_BBDD=${out_grid}_filter_bbdd.csv
 FILTER_QUERY=${out_grid}_filter_query.csv
@@ -16,5 +16,5 @@ execute_script()
 	TAG=`echo $(basename $BASH_SOURCE)`
 	sed  -e "s ${TOKEN_INPUT} ${query} g" -e "s ${TOKEN_OUTPUT} ${DRAGON_OUT} g" ${DRAGON_CFG}	 > ${out_aux}.drs
 	execute "${DRAGON_RUN} -s ${out_aux}.drs &> ${out_aux}.ucm"
-	execute "${PYTHON_RUN} ${SCRIPT_FILETER_COLUMNNS} ${target} ${DRAGON_OUT} ${OUT_MOLEC}"
+	execute "${PYTHON_RUN} ${SCRIPT_FILTER_COLUMNNS} ${target} ${DRAGON_OUT} ${OUT_MOLEC}"
 }
