3d45abc58bfdde6e0d8a7ef12f25d5808d589036
Carlos <cmartinez1@ucam.edu>
2024-01-24 12:07:20 +0100

    Update get_time_resume.sh

--- MetaScreener/extra_metascreener/used_by_metascreener/get_time_resume.sh
+++ MetaScreener/extra_metascreener/used_by_metascreener/get_time_resume.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 echo "Runs:"
 total=()
-for job in $(ls $1/*-*.err)
+for job in $1/*-*.err
 do
-  lines=$(cat $job | grep -E '([0-9]{2}:){2}[0-9]{2}')
+  lines=$(grep -E '([0-9]{2}:){2}[0-9]{2}' "$job")
   time=()
   for word in $lines
   do
-     if [[ -n $(echo ${word} | grep -E '([0-9]{2}:){2}[0-9]{2}') ]]; then
-     	time+=( $word )
-     fi
+    if [[ -n $(echo "${word}" | grep -E '([0-9]{2}:){2}[0-9]{2}') ]]; then
+      time+=( "$word" )
+    fi
   done
-  total+=($((($(date -d ${time[1]} "+%s") - $(date -d ${time[0]} "+%s")) )))
-  echo "- $(basename ${job} .err)-- ${total[-1]} s"
+  total+=( $(( ($(date -d "${time[1]}" "+%s") - $(date -d "${time[0]}" "+%s")) )) )
+  echo "- $(basename "${job}" .err)-- ${total[-1]} s"
 done
 
 sum=0
-for i in ${total[@]}; do
-  let sum+=$i
+for i in "${total[@]}"; do
+  (( sum += i ))
 done
 
 echo "Total: ${sum}"
 
 if [[ $1 != *"_LS_"* ]]; then
   echo -e "\nget_histogram_picture:"
-  cat $1/post.out | grep -E '[0-9]+.[0-9]+ s' | sed s/"End "//g
+  grep -E '[0-9]+.[0-9]+ s' "$1/post.out" | sed s/"End "//g
 fi
 
-sum_h=$(cat $1/post.out | grep "Finished experiment" | grep -Eo '[0-9]*\.?[0-9]*')
+sum_h=$(grep "Finished experiment" "$1/post.out" | grep -Eo '[0-9]*\.?[0-9]*')
 
 IFS=$'\n'
 max=$(echo "${total[*]}" | sort -nr | head -n1)
 
-echo -e "\n Real time:" $(echo $max + $sum_h | bc)
+echo -e "\n Real time:" "$(( max + sum_h ))"
 
-echo -e "\n CPU time:" $(echo $sum + $sum_h | bc)
+echo -e "\n CPU time:" "$(( sum + sum_h ))"
