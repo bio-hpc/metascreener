03cf52d6b0d7563fa57e666824a3f631bd11dcd6
Carlos <cmartinez1@ucam.edu>
2025-01-14 10:17:44 +0100

    Update conv_to.py

--- MetaScreener/extra_metascreener/convert/conv_to.py
+++ MetaScreener/extra_metascreener/convert/conv_to.py
@@ -1,8 +1,6 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-   Author: Jorge de la Peña García
-   Email:  jpena@ucam.edu
    Description: Convert molecule folders between sdf, pdb, pdbqt and mol2 formats
 
 """
