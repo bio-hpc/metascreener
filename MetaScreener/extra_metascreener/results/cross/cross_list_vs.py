b5e3a8ca3ec79660b5e59228d578bd38e1c6e35b
Carlos <cmartinez1@ucam.edu>
2025-01-14 10:19:18 +0100

    Update cross_list_vs.py

--- MetaScreener/extra_metascreener/results/cross/cross_list_vs.py
+++ MetaScreener/extra_metascreener/results/cross/cross_list_vs.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#   Author: Jorge de la Peña García
 #   Author: Carlos Martínez Cortés
 #   Email:  cmartinez1@ucam.edu
 #   Description: Examine multiples VSs and generates a resume of the results.
